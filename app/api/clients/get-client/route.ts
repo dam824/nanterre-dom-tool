c7f43143aa001ddfe6de6c8313a9c3a40776414e
dam824 <damiendagory@gmail.com>
2025-01-27 18:16:35 +0100

    maj cache

--- app/api/clients/get-client/route.ts
+++ app/api/clients/get-client/route.ts
@@ -1,10 +1,16 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from '../../../../lib/prisma';
+import { headers } from "next/headers";
 
 export async function GET(req: NextRequest) {
     try {
         const clients = await prisma.client.findMany();
-        return NextResponse.json(clients);
+        return NextResponse.json(clients, {
+            headers: {
+                "Cache-controle" : "no-store",
+            }
+        });
+       
     } catch (error) {
         console.error('Error fetching clients:', error);
         return NextResponse.json({ error: 'Error fetching clients' }, { status: 500 });
