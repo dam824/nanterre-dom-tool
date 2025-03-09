9f248112ea2b756875c9c44aa29464f760d3d892
dam824 <damiendagory@gmail.com>
2025-03-09 14:25:47 +0100

    update balance api cache issue

--- app/api/octopush/check-balance/route.ts
+++ app/api/octopush/check-balance/route.ts
@@ -1,6 +1,7 @@
 // /app/api/octopush/check-balance/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import axios from 'axios';
+import { revalidateTag } from 'next/cache';
 
 const OCTOPUSH_API_LOGIN = process.env.OCTOPUSH_API_LOGIN!;
 const OCTOPUSH_API_KEY = process.env.OCTOPUSH_API_KEY!;
@@ -15,18 +16,25 @@ export async function GET(req: NextRequest) {
     };
 
     const response = await axios.get(OCTOPUSH_API_URL, { headers });
-    console.log('API full response:', response.data); // Log the entire API response
+     
 
     // Check available keys in the response and log them
     const availableKeys = Object.keys(response.data);
-    console.log('Available keys in response:', availableKeys);
+    
 
     const amount = response.data.amount; // Ensure this key is correct.
     const unit = response.data.unit;
+    revalidateTag('balance');
 
     return NextResponse.json({
       amount,
       unit
+    }, {
+      headers: {
+          'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          'Pragma': 'no-cache',
+          'Expires': '0'
+      }
     });
   } catch (error) {
     console.error('Error fetching balance:', error);
